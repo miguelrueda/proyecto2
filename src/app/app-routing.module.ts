297010fec1ecdfd8cdd3f17494b3bff976b55ec7
greyss <greyss1@yahoo.es>
2019-01-31 20:39:53 -0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,55 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
+import { ReadComponent } from './components/read/read.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { ActorsComponent } from './components/tabs/actors/actors.component';
+import { MoviesComponent } from './components/tabs/movies/movies.component';
+import { CategoriesComponent } from './components/tabs/categories/categories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'create', component: CreateComponent,
+    children: [
+      { path: 'actor', component: ActorsComponent },
+      { path: 'movie', component: MoviesComponent },
+      { path: 'category', component: CategoriesComponent },
+      { path: '**', pathMatch: 'full', redirectTo: 'actor' }
+    ]
+  },
+  {
+    path: 'read', component: ReadComponent,
+    children: [
+      { path: 'actor', component: ActorsComponent },
+      { path: 'movie', component: MoviesComponent },
+      { path: 'category', component: CategoriesComponent },
+      { path: '**', pathMatch: 'full', redirectTo: 'actor' }
+    ]
+  },
+  {
+    path: 'update', component: UpdateComponent,
+    children: [
+      { path: 'actor', component: ActorsComponent },
+      { path: 'movie', component: MoviesComponent },
+      { path: 'category', component: CategoriesComponent },
+      { path: '**', pathMatch: 'full', redirectTo: 'actor' }
+    ]
+  },
+  {
+    path: 'delete', component: DeleteComponent,
+    children: [
+      { path: 'actor', component: ActorsComponent },
+      { path: 'movie', component: MoviesComponent },
+      { path: 'category', component: CategoriesComponent },
+      { path: '**', pathMatch: 'full', redirectTo: 'actor' }
+    ]
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'create' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { CreateComponent } from './components/create/create.component';
+import { ReadComponent } from './components/read/read.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { ActorsComponent } from './components/tabs/actors/actors.component';
+import { MoviesComponent } from './components/tabs/movies/movies.component';
+import { CategoriesComponent } from './components/tabs/categories/categories.component';
+import { SearchComponent } from './components/shared/search/search.component';
+import { CardComponent } from './components/shared/card/card.component';
+import { FormsComponent } from './components/shared/forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CreateComponent,
+    ReadComponent,
+    UpdateComponent,
+    DeleteComponent,
+    ActorsComponent,
+    MoviesComponent,
+    CategoriesComponent,
+    SearchComponent,
+    CardComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
